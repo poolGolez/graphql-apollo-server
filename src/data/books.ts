0c6929eb9b6965d8d81874679128961cc09bf78c
pool <loop.edward@gmail.com>
2023-10-18 20:10:01 +0800

    Add data for books

---
+++ src/data/books.ts
@@ -0,0 +1,26 @@
+export const books = [
+  {
+    id: 1,
+    name: "Lord of the Rings",
+  },
+  {
+    id: 2,
+    name: "The Hobbit",
+  },
+  {
+    id: 3,
+    name: "Prelude to Foundation",
+  },
+  {
+    id: 4,
+    name: "Foundation",
+  },
+  {
+    id: 5,
+    name: "Foundation and Empire",
+  },
+  {
+    id: 6,
+    name: "Forward to Foundation",
+  },
+];
